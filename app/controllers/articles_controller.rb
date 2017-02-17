692d7b79eb91920dec9fccd66eba114d81fa6265
Philip Jones <knettwerkproject@gmail.com>
2017-02-17 05:23:06 +0000

    Style form partial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -49,6 +49,7 @@ class ArticlesController < ApplicationController
     def set_article
       @article = Article.find(params[:id])
     end
+    
     def article_params
       params.require(:article).permit(:title, :description)
     end
